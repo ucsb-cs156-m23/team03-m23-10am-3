eb8b6b007f7dbe4bd1abc7bb37a79e347d1b31c6
andrew <andrewyu6581@gmail.com>
2023-08-08 14:48:20 -0700

    ay add menuitemreview fixtures

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,41 @@
+const menuItemReviewFixtures = {
+    oneMenuItemReview: [
+        {
+            "id": 1,
+            "itemId": 1,
+            "reviewerEmail": "test@ucsb.edu",
+            "stars": 5,
+            "dateReviewed": "2023-01-01T00:00:00.000Z",
+            "comments": "Test"
+        }
+    ],
+
+    threeMenuItemReviews: [
+        {
+            "id": 2,
+            "itemId": 1,
+            "reviewerEmail": "a@ucsb.edu",
+            "stars": 1,
+            "dateReviewed": "2023-01-01T00:00:00.000Z",
+            "comments": "First"
+        },
+        {
+            "id": 3,
+            "itemId": 2,
+            "reviewerEmail": "b@ucsb.edu",
+            "stars": 2,
+            "dateReviewed": "2023-01-02T00:00:00.000Z",
+            "comments": "Second"
+        },
+        {
+            "id": 4,
+            "itemId": 3,
+            "reviewerEmail": "c@ucsb.edu",
+            "stars": 3,
+            "dateReviewed": "2023-01-03T00:00:00.000Z",
+            "comments": "Third"
+        }
+    ]
+};
+
+export { menuItemReviewFixtures };
